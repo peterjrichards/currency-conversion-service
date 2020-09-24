ae7d3f06c2238c4c26076321815bc0ab3396e299
Peter Richards <peter.richards@thefuturetoday.net>
2020-09-24 20:42:38 +0100

    Added Spring Sleuth

--- src/main/java/net/thefuturetoday/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ src/main/java/net/thefuturetoday/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -1,9 +1,11 @@
 package net.thefuturetoday.microservices.currencyconversionservice;
 
+import brave.sampler.Sampler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableFeignClients("net.thefuturetoday.microservices.currencyconversionservice")
@@ -14,4 +16,10 @@ public class CurrencyConversionServiceApplication {
 		SpringApplication.run(CurrencyConversionServiceApplication.class, args);
 	}
 
+	@Bean
+	//creating a sampler called always sampler
+	public Sampler defaultSampler()
+	{
+		return Sampler.ALWAYS_SAMPLE;
+	}
 }
